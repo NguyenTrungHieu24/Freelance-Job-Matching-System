d72d2447a794110839f8c428d1802d69852e1e3d
DucHT <ductranhoangk35dlnq@gmail.com>
2026-05-21 00:20:24 +0700

    Add Skill + Category api controller

---
+++ API/Controllers/SkillsController.cs
@@ -0,0 +1,121 @@
+﻿using BusinessObjects;
+using BusinessObjects.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/skills")]
+    [ApiController]
+    public class SkillsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public SkillsController(
+            AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/skills
+        [HttpGet("/")]
+        public async Task<ActionResult<IEnumerable<Skill>>>
+            GetSkills()
+        {
+            var skills = await _context.Skills
+                .OrderBy(x => x.Name)
+                .ToListAsync();
+
+            return Ok(skills);
+        }
+
+        // GET: api/skills/1
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Skill>>
+            GetSkill(int id)
+        {
+            var skill = await _context.Skills
+                .FindAsync(id);
+
+            if (skill == null)
+            {
+                return NotFound(new
+                {
+                    message = "Skill not found"
+                });
+            }
+
+            return Ok(skill);
+        }
+
+        // POST: api/skills
+        [HttpPost]
+        public async Task<ActionResult<Skill>>
+            CreateSkill([FromBody] Skill skill)
+        {
+            _context.Skills.Add(skill);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                nameof(GetSkill),
+                new { id = skill.Id },
+                skill
+            );
+        }
+
+        // PUT: api/skills/1
+        [HttpPut("{id}")]
+        public async Task<ActionResult>
+            UpdateSkill(
+                int id,
+                [FromBody] Skill updatedSkill)
+        {
+            var skill = await _context.Skills
+                .FindAsync(id);
+
+            if (skill == null)
+            {
+                return NotFound(new
+                {
+                    message = "Skill not found"
+                });
+            }
+
+            skill.Name = updatedSkill.Name;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(new
+            {
+                message = "Skill updated successfully"
+            });
+        }
+
+        // DELETE: api/skills/1
+        [HttpDelete("{id}")]
+        public async Task<ActionResult>
+            DeleteSkill(int id)
+        {
+            var skill = await _context.Skills
+                .FindAsync(id);
+
+            if (skill == null)
+            {
+                return NotFound(new
+                {
+                    message = "Skill not found"
+                });
+            }
+
+            _context.Skills.Remove(skill);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(new
+            {
+                message = "Skill deleted successfully"
+            });
+        }
+    }
+}
