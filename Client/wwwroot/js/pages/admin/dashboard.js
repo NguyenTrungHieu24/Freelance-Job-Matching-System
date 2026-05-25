11e328069ef6e729fbdd31c0cc4a6a4b2436fef6
DucHT <ductranhoangk35dlnq@gmail.com>
2026-05-25 21:27:24 +0700

    Update: Dashboard minichart

--- Client/wwwroot/js/pages/admin/dashboard.js
+++ Client/wwwroot/js/pages/admin/dashboard.js
@@ -1,6 +1,7 @@
 ﻿let userGrowthChart;
 let jobGrowthChart;
 
+
 document.addEventListener("DOMContentLoaded", async function () {
 
     await loadDashboard();
@@ -14,6 +15,16 @@ document.addEventListener("DOMContentLoaded", async function () {
         });
 });
 
+/**
+ * @desc Random array
+ * @param {any} l Array length
+ * @param {any} m max value of array item
+ * @returns
+ */
+const randArr = (l = 10, m = 100) => Array.from(
+    { length: l },
+    () => Math.floor(Math.random() * m) + 1
+);
 
 const hideSkeleton = (name) => {
     $(`#${name}Loading`).addClass("d-none");
@@ -21,7 +32,7 @@ const hideSkeleton = (name) => {
 };
 
 const showSkeletons = (names = []) => {
-    for(let name of names) {
+    for (let name of names) {
         showSkeleton(name);
     }
 }
@@ -55,18 +66,30 @@ async function loadOverview(range) {
     hideSkeleton("users");
     $("#totalUsers").text(data.userStats.totalUsers);
     $("#newUsers").text(`+${data.userStats.totalNewUsers} new users`);
+    renderMiniLineChart('#miniChartUsers', {
+        data: data.userStats?.miniChart ?? randArr()
+    });
 
     hideSkeleton("jobs");
     $("#totalJobs").text(data.jobStats.totalJobs);
     $("#activeJobs").text(`${data.jobStats.activeJobs} active jobs`);
+    renderMiniLineChart('#miniChartJobs', {
+        data: data.jobStats?.miniChart ?? randArr()
+    });
 
     hideSkeleton("applications");
     $("#totalApplications").text(data.applicationStats.totalApplications);
     $("#matchRate").text(`${data.applicationStats.matchRate}% match rate`);
+    renderMiniLineChart('#miniChartApplications', {
+        data: data.applicationStats?.miniChart ?? randArr()
+    });
 
     hideSkeleton("revenue");
     $("#totalRevenue").text(`$${data.revenueStats.totalRevenue}`);
     $("#monthlyRevenue").text(`$${data.revenueStats.revenueInRange} this month`);
+    renderMiniLineChart('#miniChartRevenue', {
+        data: data.revenueStats?.miniChart ?? randArr()
+    });
 }
 
 async function loadUserGrowthChart(range) {
@@ -202,4 +225,58 @@ function formatDate(dateString) {
     const date = new Date(dateString);
 
     return date.toLocaleDateString();
+}
+
+let miniCharts = new Map();
+
+/**
+ * @desc Render Mini chart
+ * @param {string} elem_id Dom element id
+ * @param {{data: [], color?: string, background?: string}} options
+ * @returns
+ */
+const renderMiniLineChart = (elem_id, options) => {
+    if (miniCharts.has(elem_id)) {
+        miniCharts.get(elem_id).destroy();
+    }
+
+    miniCharts.set(elem_id,
+        new Chart(
+            $(elem_id).get(0),
+            {
+                type: 'line',
+                data: {
+                    labels: options.data.map(e => ''),
+                    datasets: [
+                        {
+                            data: options.data,
+                            borderWidth: 2,
+                            tension: 0.4,
+                            pointRadius: 0,
+                            fill: true,
+
+                            borderColor: options?.color ?? "#139139",
+
+                            backgroundColor: options?.background ?? 'rgba(22,163,74,0.15)',
+                        }
+                    ]
+                },
+                options: {
+                    plugins: {
+                        legend: {
+                            display: false
+                        }
+                    },
+                    scales: {
+                        x: {
+                            display: false
+                        },
+                        y: {
+                            display: false
+                        }
+                    }
+                }
+            }
+        )
+    );
 }
