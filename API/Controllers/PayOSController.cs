d7265a3f0d3e182953db02f513850e4ef7ec3ddb
DucHT <ductranhoangk35dlnq@gmail.com>
2026-05-28 00:37:08 +0700

    Validate PayOS webhook request

--- API/Controllers/PayOSController.cs
+++ API/Controllers/PayOSController.cs
@@ -1,11 +1,16 @@
-﻿using API.Services.Auth;
+﻿using API.Configurations;
+using API.Helper;
+using API.Services.Auth;
 using AutoMapper;
 using BusinessObjects;
 using BusinessObjects.Common;
 using BusinessObjects.Enums;
+using BusinessObjects.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using NuGet.Configuration;
 
 namespace API.Controllers
 {
@@ -13,28 +18,48 @@ namespace API.Controllers
     [ApiController]
     public class PayOSController : BaseController
     {
-        public PayOSController(AppDbContext context, IMapper mapper, IUserService user) : base(context, mapper, user)
+
+        private readonly PayOSSettings _settings;
+        private readonly ILogger<PayOSController> _logger;
+
+        public PayOSController(AppDbContext context, IMapper mapper, IUserService user, IOptions<PayOSSettings> settings, ILogger<PayOSController> logger) : base(context, mapper, user)
         {
+            _settings = settings.Value;
+            _logger = logger;
         }
 
         [HttpPost("webhook")]
-        public async Task<IActionResult> HandleWebhook([FromBody] PayOSWebhook webhook)
+        public async Task<IActionResult> HandleWebhook()
         {
-            // 1. log raw data
-            Console.WriteLine($"Webhook received: {webhook.OrderCode}");
+            using var reader = new StreamReader(Request.Body);
+            var rawBody = await reader.ReadToEndAsync();
 
-            // 2. find payment
+            var signature = Request.Headers["x-payos-signature"].ToString();
+
+            // 3. verify
+            var isValid = PayOSSignatureHelper.Verify(
+                rawBody,
+                signature,
+                _settings.ChecksumKey
+            );
+
+            if (!isValid)
+            {
+                _logger.LogWarning("Invalid PayOS webhook signature");
+                return Unauthorized();
+            }
+
+            var webhook = System.Text.Json.JsonSerializer.Deserialize<PayOSWebhook>(rawBody);
+
             var payment = await _context.Payments
                 .FirstOrDefaultAsync(x => x.TransactionCode == webhook.OrderCode.ToString());
 
             if (payment == null)
                 return Ok(); // tránh retry spam
 
-            // 3. update status
             payment.Status = webhook.Status == "SUCCESS" ? PaymentStatus.PAID : PaymentStatus.FAILED; // SUCCESS / FAILED
             payment.PaidAt = DateTime.Now;
 
-            // 4. update application
             var application = await _context.Applications
                 .FindAsync(payment.ApplicationId);
 
@@ -45,7 +70,9 @@ namespace API.Controllers
 
             await _context.SaveChangesAsync();
 
-            return Ok();
+            _logger.LogInformation($"Payment success: {webhook?.OrderCode}");
+
+            return Ok(new { success = true });
         }
     }
 }
