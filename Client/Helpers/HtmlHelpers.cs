9e672926ec2f7ffbd2159a18ce615d6aa1707032
DucHT <ductranhoangk35dlnq@gmail.com>
2026-06-01 23:00:36 +0700

    Add HTML Helpers

---
+++ Client/Helpers/HtmlHelpers.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Client.Helpers
+{
+    public static class HtmlHelpers
+    {
+        public static string IsActive(this IHtmlHelper html,
+            string controller,
+            string action = null)
+        {
+            var routeData = html.ViewContext.RouteData;
+
+            var currentController = routeData.Values["Controller"]?.ToString();
+            var currentAction = routeData.Values["Action"]?.ToString();
+
+            if (currentController == controller &&
+                (action == null || currentAction == action))
+            {
+                return "is-active";
+            }
+
+            return "";
+        }
+    }
+}
---
+++ Client/Models/NavViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Client.Models
+{
+    public class NavViewModel
+    {
+        public string Title {  get; set; }
+        public string Controller {  get; set; }
+        public string? Action { get; set; } = "Index";
+        public string? Icon { get; set; } = "fa-chart-line";
+    }
+}
