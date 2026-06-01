65bc4d4ced7b1a1cb700fb093b88de240fff71df
DucHT <ductranhoangk35dlnq@gmail.com>
2026-06-01 23:26:39 +0700

    Update: Nav Bar

--- Client/Models/NavViewModel.cs
+++ Client/Models/NavViewModel.cs
@@ -2,9 +2,13 @@
 {
     public class NavViewModel
     {
-        public string Title {  get; set; }
-        public string Controller {  get; set; }
+        public string Title { get; set; }
+        public string Controller { get; set; }
         public string? Action { get; set; } = "Index";
         public string? Icon { get; set; } = "fa-chart-line";
+
+        public List<NavViewModel>? Children { get; set; } = new();
+
+        public bool HasChildren => (Children != null && Children.Count > 0);
     }
 }
