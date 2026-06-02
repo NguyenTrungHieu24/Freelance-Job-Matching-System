0afeaddb455b91d38b589a604dfadc4cf08516a9
DucHT <ductranhoangk35dlnq@gmail.com>
2026-06-02 21:11:35 +0700

    Test

---
+++ Client/Controllers/JobController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Client.Controllers
+{
+    public class JobController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public JobController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetAsync(
+                "https://localhost:7001/api/jobs"
+            );
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return View(new List<string>());
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+
+            var jobs = JsonSerializer.Deserialize<List<string>>(json);
+
+            return View(jobs);
+        }
+    }
+}
