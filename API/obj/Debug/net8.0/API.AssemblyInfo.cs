7fb8dc04c3fcd926214dffb7bba66ff410205aeb
Duong <duongncthe161851@fpt.edu.vn>
2026-06-30 11:43:29 +0700

    Sua loi code do merge nhanh

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fd7caab3703e62e427f146df8d4121027cace6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ac4167a8aa24b14d8f8be9a61f9fe5f147da00b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
